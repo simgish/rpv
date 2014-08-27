adda2b757295a2e74600e19b4acb961e5146cd6b
Ryan Doughty <ryandoughty@gmail.com>
2014-08-26 20:11:37 -0400

    added deps. add api foundation

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-var http = require("http"),
-    mongojs = require("mongojs");
+var express = require("express"),
+    app = express(),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
+// mongoose.connect('mongodb://mars:Winter12@ds063869.mongolab.com:63869');
+mongoose.connect('mongodb://localhost/test');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+// app.use(methodOverride());
+
+var MarsSchema = new mongoose.Schema({
+  order: Number,
+  name: String,
+  days: Number
+});
+
+var Mars = mongoose.model('orders', MarsSchema);
+
+app.get('/gettrips', function(req, res) {
+  Mars.find(function(err, orders) {
+    if (err) {
+      res.send(err);
+    }
+
+    res.json(orders);
+  });
+});
+
+app.listen(8989);
+
+console.log('api listening on 8989');
