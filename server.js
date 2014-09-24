6eb3962fac97d819080dce6ef1c194bf682a469a
simgish <ryandoughty@gmail.com>
2014-09-24 09:29:45 -0400

    allow all headers

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ app.use(bodyParser.urlencoded({'extended': 'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE');
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+    if ('OPTIONS' == req.method){
+        return res.send(200);
+    }
+    next();
+});
+
 var ClientSchema = new mongoose.Schema({
   contactName: String,
   dba: String,
